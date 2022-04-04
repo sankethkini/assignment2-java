4d4a5e96bc3b8fdfc67be5390b1f1a6e25012e19
sankethkini <kinisanketh@gmail.com>
2022-04-04 19:40:50 +0530

    infinite loop logic changed by using boolean variable

--- src/main/java/assign2/Application.java
+++ src/main/java/assign2/Application.java
@@ -8,62 +8,70 @@ import assign2.services.memory.Memory;
 import assign2.services.memory.MemoryImpl;
 import assign2.services.storage.FileStorage;
 import assign2.services.storage.Storage;
-import assign2.services.userservice.*;
+import assign2.services.userservice.Delete;
+import assign2.services.userservice.Display;
+import assign2.services.userservice.Exit;
+import assign2.services.userservice.InteractiveInput;
+import assign2.services.userservice.Save;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Application {
-    private Storage storage;
-    private Memory memory;
-    public Application() throws FileReadException {
-        this.storage=new FileStorage(FileConstants.fileName);
-        this.memory=new MemoryImpl();
-        List<User> users=storage.read();
-        if(users!=null && users.size()!=0){
-            User[] arr=users.toArray(new User[0]);
-            this.memory.save(arr);
+  private final Storage storage;
+  private final Memory memory;
+
+  public Application() throws FileReadException {
+    this.storage = new FileStorage(FileConstants.fileName);
+    this.memory = new MemoryImpl();
+    List<User> users = storage.read();
+    if (Objects.nonNull(users) && users.size() != 0) {
+      User[] arr = users.toArray(new User[0]);
+      this.memory.save(arr);
+    }
+  }
+
+  public void run() {
+    try (Scanner sc = new Scanner(System.in)) {
+      boolean cont=true;
+      do {
+        System.out.println(DisplayConstants.line);
+        System.out.print(
+            "Select a option\n"
+                + "1. Add new user\n"
+                + "2. Display all users\n"
+                + "3. Delete user\n"
+                + "4. Save user details\n"
+                + "5. Exit"
+        );
+        int option = sc.nextInt();
+        sc.nextLine();
+        switch (option) {
+          case 1:
+            InteractiveInput.run(sc, memory);
+            break;
+          case 2:
+            Display.run(sc, memory);
+            break;
+          case 3:
+            Delete.run(sc, memory);
+            break;
+          case 4:
+            Save.run(storage, memory);
+            break;
+          case 5:
+            Exit.run(sc, storage, memory);
+            cont=false;
+            break;
+          default:
+            System.out.println("enter the correct option");
         }
-    }
+        System.out.println(DisplayConstants.line);
+      } while (cont);
 
-    public void run(){
-            try (Scanner sc=new Scanner(System.in)){
-                while (true){
-                    System.out.println(DisplayConstants.line);
-                    System.out.print(
-                            "Select a option\n"+
-                                    "1. Add new user\n"+
-                                    "2. Display all users\n"+
-                                    "3. Delete user\n"+
-                                    "4. Save user details\n"+
-                                    "5. Exit"
-                    );
-                    int option=sc.nextInt();
-                    sc.nextLine();
-                    switch (option){
-                        case 1:
-                            InteractiveInput.run(sc,memory);
-                            break;
-                        case 2:
-                            Display.run(sc,memory);
-                            break;
-                        case 3:
-                            Delete.run(sc,memory);
-                            break;
-                        case 4:
-                            Save.run(storage,memory);
-                            break;
-                        case 5:
-                            Exit.run(sc,storage,memory);
-                            break;
-                        default:
-                            System.out.println("enter the correct option");
-                    }
-                    System.out.println(DisplayConstants.line);
-                }
-
-            } catch (Exception e){
-                System.out.println(e.getMessage());
-                System.exit(0);
-            }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      System.exit(0);
     }
+  }
 }
